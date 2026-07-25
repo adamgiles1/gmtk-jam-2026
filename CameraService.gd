fc95a7c177004193a49ef2202ea7722f6a3c8908
Adam Giles <adamag@cox.net>
2026-07-25 00:07:53 -0400

    STuff

---
+++ CameraService.gd
@@ -0,0 +1,29 @@
+extends Node
+
+var photo_size := Vector2i(200, 150)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_text_backspace"):
+		take_photo(get_viewport().get_visible_rect().size / 2)
+
+func take_photo(position: Vector2i) -> void:
+	print("taking photo at position: ", position)
+	await RenderingServer.frame_post_draw
+	var screen: Image = get_viewport().get_texture().get_image()
+	var viewport_size := get_viewport().get_visible_rect().size
+	var crop_pos := position - photo_size / 2
+	
+	var img := Image.create(photo_size.x, photo_size.y, false, screen.get_format())
+	img.blit_rect(screen, Rect2i(crop_pos, photo_size), Vector2i.ZERO)
+	
+	save_image(img)
+
+func save_image(img: Image) -> void:
+	var desktop = OS.get_system_dir(OS.SYSTEM_DIR_DESKTOP)
+	var filename = "adam-test-%s.png" % Time.get_datetime_string_from_system().replace(":", "-")
+	var path = desktop.path_join(filename)
+	var err = img.save_png(path)
+	if err == OK:
+		print("photo saved")
+	else:
+		print("photo failed to save")
---
+++ tools/camera.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var box: ColorRect = $ColorRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var mouse_pos = get_global_mouse_position()
+	global_position = mouse_pos - box.size / 2
+	
+	if Input.is_action_just_pressed("take_photo"):
+		take_photo()
+
+func take_photo() -> void:
+	print("taking photo")
+	CameraService.take_photo(Vector2i(global_position + box.size / 2))
+	
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	queue_free()
